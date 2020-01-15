71dd0c13be9c714738caaa1ed5c1af768939aba4
curto2 <curto.2@my.cityu.edu.hk>
2020-01-16 00:17:38 +0100

    Dr. of Crosswise

---
+++ README.md
@@ -0,0 +1,33 @@
+# Dr. of Crosswise
+
+Doctor of Crosswise: Reducing Over-parametrization in Neural Networks.
+
+<p align="center">
+<img src="dr_of_crosswise.png" width="400">
+</p>
+
+For more details about the publication, visit the website:
+
+  http://decurto.ch
+
+If you find this example useful, please cite the paper below:
+
+        @article{Curto19,
+          author = "J. D. Curt\'o and I. C. Zarza and K. Kitani and I. King and M. R. Lyu",
+          title = "Doctor of Crosswise: Reducing Over-parametrization in Neural Networks",
+          journal = "arXiv:1905.10324",
+          year = "2019",
+        }
+
+To run the code, enter the given folder and run:
+
+	$ pip3 install -U scikit-learn
+
+	$ python c_and_z.py
+
+--------------------------------------------------------
+File Information
+--------------------------------------------------------
+
+- c_and_z.py
+  - Toy example demonstrating the use of the newly developed operand. A network motivated by <a href="https://github.com/curto2/mckernel">McKernel</a> is used. Weights are kept random, for simplicity.
---
+++ c_and_z.py
@@ -0,0 +1,77 @@
+#   Doctor of Crosswise: Reducing Over-parametrization in Neural Networks.		    
+#										
+#   Authors: Curtó and Zarza.
+#   c@decurto.ch z@dezarza.ch 						    
+
+import numpy as c
+import time as t
+from sklearn import metrics
+from sklearn.datasets import fetch_mldata as ml
+from sklearn.ensemble import RandomForestClassifier
+
+ratio_of_features = 2;
+
+def selu(C):
+	C = c.multiply(1.0507009873554804934193349852946, C)
+	C[C <= 0] = c.multiply(1.6732632423543772848170429916717, c.expm1(C[C <= 0]))
+	return C
+
+def c_and_z(A, B):
+	M = A.shape[0] 
+	N = B.shape[0] 
+	C = c.zeros((M, A.shape[1] * N))
+	Z = c.zeros((N, A.shape[1]))
+	for m in range(M):
+		for n in range(N):
+			Z[n, :] = c.multiply(A[m, :], B[n, :])
+		C[m, :] = Z.flatten()
+	return C
+
+def run():
+	mnist = ml('MNIST original')
+	n_train = 60000
+	n_test = 10000
+
+	c.random.seed(7)
+
+	# Define sets of train and test.
+	train = c.arange(0, n_train)
+	test = c.arange(n_train + 1, n_train + n_test)
+
+	X_train, Y_train = mnist.data[train], mnist.target[train]
+	X_test, Y_test = mnist.data[test], mnist.target[test]
+
+	# Apply an algorithm of learning.
+	print("Applying an algorithm of learning.")
+	clr = RandomForestClassifier(n_estimators = 100, n_jobs = -1)
+
+	K = c.random.randn(ratio_of_features, X_train.shape[1])
+	b = c.random.randn(X_train.shape[1] * ratio_of_features)
+
+	x1 = selu(c_and_z(X_train, K) + b)
+	K1 = c.random.randn(2, x1.shape[1])
+	b1 = c.random.randn(2 * x1.shape[1])
+
+	x2 = selu(c_and_z(x1, K1) + b1)
+	K2 = c.random.randn(x2.shape[1])
+	b2 = c.random.randn(x2.shape[1])
+
+	clr.fit(selu(c.multiply(x2, K2) + b2), Y_train)
+
+	# Make a prediction.
+	print("Making predictions.")
+
+	X_t1 = selu(c_and_z(X_test, K) + b)
+	X_t2 = selu(c_and_z(X_t1, K1) + b1)
+
+	Y_p = clr.predict(selu(c.multiply(X_t2, K2) + b2))
+
+	# Evaluate the prediction.
+	print("Evaluating results...")
+	print("Precision: \t \t", "{:.2f}".format(metrics.accuracy_score(Y_test, Y_p) * 100), "%.")
+
+if __name__ == "__main__":
+	start = t.time()
+	run()
+	end = t.time()
+	print("Overall running time: \t", "{:.2f}".format(end - start), "s.")
